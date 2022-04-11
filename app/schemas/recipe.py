fc3d1c7dbc73bf88fb96120545fa719d039653e6
Imanuel Febie <tuffgniuz.dev@gmail.com>
2022-04-11 14:01:18 +0700

    modified recipe schema

--- app/schemas/recipe.py
+++ app/schemas/recipe.py
@@ -5,34 +5,40 @@ import uuid
 from typing import Optional
 from bson.objectid import ObjectId
 from pydantic import BaseModel, Field
-from datetime import date 
+from datetime import date
+
+from app.schemas.category import CategorySchema
+
 
 class RecipeSchema(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias='_id')
     title: str = Field(...)
     description: str = Field(...)
     cooking_time: int = Field(...)
-    attachment: str = Field(...)
-    create_at: date = Field(...)
-    update_at: Optional[date]
-    category: Optional[list] #(Dictionary,Json,EMBEDDED,REFRENCE)
-    ingridient: Optional[list]  #(Dictionary,JSON,EMBEDDED,REFERENCE)
-    bookmarks: Optional[list] #(Dictionary,Json,EMBEDDED,REFRENCE)
+    image: Optional[str] = None  # path to image
+    video: Optional[str] = None  # path to videofile
+    created_at: date = Field(...)
+    updated_at: Optional[date]
+    # categories: Optional[list[CategorySchema]] = None
+    # ingridient: Optional[list]  # (Dictionary,JSON,EMBEDDED,REFERENCE)
+    # bookmarks: Optional[list]  # (Dictionary,Json,EMBEDDED,REFRENCE)
 
-
-    class Config: 
+    class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         schema_extra = {
-            'example':{
-                'title':'Mushroom Pizza',
-                'description':'Very Yummy Mushroom Pizza',
+            'example': {
+                'title': 'Mushroom Pizza',
+                'description': 'Very Yummy Mushroom Pizza',
                 'cooking_time': 10,
-                'attachment': 'mushroompizza.jpg',
-                'create_at': date.today()
-            } #Schema 
+                'image': '',
+                'video': '',
+                'categories': '',
+                'created_at': date.today()
+            }  # Schema
         }
-    
+
+
 class RecipeUpdateSchema(BaseModel):
     title: Optional[str]
     description: Optional[str]
@@ -45,23 +51,14 @@ class RecipeUpdateSchema(BaseModel):
     #Bookmarks: Optional[array]
 
     class Config:
-            arbitrary_types_allowed = True
-            json_encoders = {ObjectId: str}
-            schema_extra = {
-        'example':{
-            'title':'Mushroom Pizza',
-            'description':'Very Yummy Mushroom Pizza',
-            'cooking_time': 10,
-            'attachment': 'mushroompizza.jpg',
-            'updated_at': date.today()
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            'example': {
+                'title': 'Mushroom Pizza',
+                'description': 'Very Yummy Mushroom Pizza',
+                'cooking_time': 10,
+                'attachment': 'mushroompizza.jpg',
+                'updated_at': date.today()
+            }
         }
-    }
-
-
-
-
-
-
-    
-
-
