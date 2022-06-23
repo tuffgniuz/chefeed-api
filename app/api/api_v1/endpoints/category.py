940a74cde09a58fe4852ec7305bd89cb327cc37b
Imanuel Febie <tuffgniuz.dev@gmail.com>
2022-06-23 19:54:17 +0700

    deleted description from category collection

--- app/api/api_v1/endpoints/category.py
+++ app/api/api_v1/endpoints/category.py
@@ -7,36 +7,46 @@ from starlette.responses import JSONResponse
 
 from ....schemas.category import CategorySchema
 
-router = APIRouter(prefix= '/api/v1/categories',tags=['categories'])
+router = APIRouter(prefix='/api/v1/categories', tags=['categories'])
 
 """RETRIEVE ALL CATEGORY"""
-@router.get('/', response_description = 'List All Categories',response_model=list[CategorySchema])
+
+
+@router.get('/', response_description='List All Categories', response_model=list[CategorySchema])
 async def retrieve_category(request: Request):
     categories = await request.app.mongodb['categories'].find().to_list(1000)
     return categories
 
 
 """RETRIEVE CATEGORY BY ID"""
+
+
 @router.get("/{id}", response_description="Get cateogry by id")
 async def retrieve_category_by_id(id: str, request: Request):
     category = await request.app.mongodb["categories"].find_one({"_id": id})
     if category is not None:
         return category
-    raise HTTPException(status_code=404, detail=f"Category with {id} is not found")
+    raise HTTPException(
+        status_code=404, detail=f"Category with {id} is not found")
 
 
 """CREATE NEW CATEGORY"""
+
+
 @router.post('/', response_description='Add new category', response_model=CategorySchema)
 async def create_category(request: Request, category: CategorySchema = Body(...)):
     category = jsonable_encoder(category)
     new_category = await request.app.mongodb['categories'].insert_one(category)
     created_category = await request.app.mongodb['categories'].find_one({'_id': new_category.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_category) 
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_category)
 
-""" DELETE CATEGORY ID """ 
+""" DELETE CATEGORY ID """
+
+
 @router.delete("/{id}", response_description="Delete category")
 async def delete_category(id: str, request: Request):
     delete_category = await request.app.mongodb["categories"].delete_one({"_id": id})
     if delete_category.deleted_count == 1:
         return "Category has been successfully deleted"
-    raise HTTPException(status_code=404, detail=f"Category with {id} is not found")
+    raise HTTPException(
+        status_code=404, detail=f"Category with {id} is not found")
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from .api.api_v1.endpoints.user import router as UserRouter
 from .api.api_v1.endpoints.recipe import router as RecipeRouter
-#from .api.api_v1.endpoints.category import router as CategoryRouter
+from .api.api_v1.endpoints.category import router as CategoryRouter
 #from .api.api_v1.endpoints.ingredients import router as IngredientRouter
 #from .api.api_v1.endpoints.bookmark import router as BookmarksRouter
 from .config import settings
@@ -26,4 +26,4 @@ async def shutdown_db_client():
 
 app.include_router(UserRouter)
 app.include_router(RecipeRouter)
-
+app.include_router(CategoryRouter)
